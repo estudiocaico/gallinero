42f884c963c0ca7564e293300bea87ff0b4dc3bf
estudiocaico <estudiocaico@gmail.com>
2026-05-22 15:29:23 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.GALLINERO_SUPABASE = {
-  url: "https://tsvfxxonveaaeurdqflbt.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzdnh4b252ZWFhZXVyZHFmbGJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0NTE4NTEsImV4cCI6MjA5NTAyNzg1MX0._ETiOXQLgpMm1EaB5VfOQTw3pIOs8-8NQpp5XdLtOPY",
+  url: "https://tsvxxonveaaeurdqflbt.supabase.co",
+  anonKey: "sb_publishable_vawEzFA6ilJluAhs-A7_JA_aMPvG27P",
   rowId: "gallinero-principal",
 };
